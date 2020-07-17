7337fe56f1afd6fbd06cfd40ac3f8f6f12f108dd
mjaquiery <matt.jaquiery@psy.ox.ac.uk>
2020-07-17 13:22:59 +0100

    Quick caption update
    Updated knitr hooks to provide a quick caption using fig.caption="Figure title.  Description"
    'Figure title.' (i.e. up to the double-space) will appear as the short caption, used by the list of figures.
    The whole fig.caption, with double-spaces replaced by \newline , will be used as the caption.
    
    General setup now cleans the workspace and loads libraries to ensure each script starts with a fresh environment and the same commands available.
    
    Updated ggplot theme slightly.

--- scripts_and_filters/general_setup.R
+++ scripts_and_filters/general_setup.R
@@ -1,24 +1,85 @@
+#' Setup script for thesis chapters
+#'
+#' - Matt Jaquiery, 16/07/2020
+#'
+#' This script loads various libraries and sets options and themes so that they
+#' are consistent in each chapter and will not be modified by changes in
+#' previous chapters.
+
+
+# Clear workspace ---------------------------------------------------------
+
+rm(list = ls())
+
+
+# Libraries ---------------------------------------------------------------
+
+library(tidyverse)  # Pipes, tidyselectors, string manipulation, data wrangling, etc.
+library(kableExtra) # Pretty formatting of output tables
+library(esmData)    # My own data package holding the data for the project (`remotes::install_github('oxacclab/esmData')`)
+library(prettyMD)   # My own formatting library for neatly formatting stats output (`remotes::install_github('mjaquiery/prettyMD)`)
+
+
+# Load specific knitr stuff -----------------------------------------------
+
+##### add chunk options for PDF output ####
+# for guidance on how to create your own chunk options see
+# https://ulyngs.github.io/blog/posts/2019-02-01-how-to-create-your-own-chunk-options-in-r-markdown/
+
+knitr::knit_hooks$set(vspace_output = function(before, options, envir) {
+  if (!before) {
+    ## after a chunk has been evaluated
+    end <- paste0("\\vspace{", options$vspace_output, "}")
+    stringr::str_c(out, end)
+  }
+})
+
+knitr::knit_hooks$set(quote_author = function(before, options, envir) {
+  if (!before) {
+    ## after a chunk has been evaluated
+    latex_include <- paste0("\\\\qauthor\\{", options$quote_author, "\\}\\1")
+    gsub('(\\\\end\\{savequote\\})', latex_include, txt)
+  }
+})
+
+#' Caption figures with a short caption (up to \newline) and longer caption thereafter
+#' Matt Jaquiery
+knitr::opts_hooks$set(
+  fig.caption = function(options) {
+    short <- str_match(options$fig.caption, '(.+?)( {2})')
+    if (!is.na(short[1]))
+      options$fig.scap <- short[2]
+    options$fig.cap <- gsub(' {2}', '\\\\newline ', options$fig.caption)
+    options
+  }
+)
+
 options(
-  tinytex.verbose = TRUE
+  tinytex.verbose = TRUE,
+  bookdown.render.file_scope = FALSE
 )
 
 knitr::opts_chunk$set(
-  echo = F, 
-  warnings = F, 
+  echo = F,
+  warnings = F,
   messages = F
 )
 
-library(ggplot2)
 
+# Plot theme --------------------------------------------------------------
+
+# ggplot basic theme
 theme_set(
   theme_light() +
     theme(
+      rect = element_blank(),
       panel.background = element_blank(),
       panel.border = element_blank(),
       panel.grid.minor = element_blank(),
       panel.grid.major = element_blank(),
-      axis.line = element_line(colour = "black"),
+      axis.line = element_line(colour = 'black'),
+      axis.ticks = element_line(colour = 'black'),
       text = element_text(size = 14),
-      legend.position = "top"
+      legend.position = 'top'
     )
 )
