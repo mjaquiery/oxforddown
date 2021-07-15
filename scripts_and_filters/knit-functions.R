09e15b8c1a27448aa1a9b85b42c037c08f3bb87f
mjaquiery <matt.jaquiery@psy.ox.ac.uk>
2021-07-15 20:38:46 +0100

    Bibliography repair and upstream integration

--- scripts_and_filters/knit-functions.R
+++ scripts_and_filters/knit-functions.R
@@ -1,28 +1,38 @@
-knit_thesis <- function(input, output_format = "pdf", ...){
+knit_thesis <- function(input, output_format = "pdf", debug = F, ...){
+  if (debug) {
+    trace(recover, sink)
+  }
+  # Save to protect against `rm()` calls
+  save(output_format, file = "_knit_thesis_output_format.rda")
+  
   if ("pdf" %in% output_format){
-    bookdown::render_book(input, output_format = "bookdown::pdf_book")
+    bookdown::render_book(input, output_format = "bookdown::pdf_book", ...)
     
     file.remove(list.files(pattern = "*\\.(log|mtc\\d*|maf|aux|bcf|lof|lot|out|toc)$"))
   }
   
+  load("_knit_thesis_output_format.rda")
   if ("bs4" %in% output_format){
-    bookdown::render_book(input, output_format = "bookdown::bs4_book")
+    bookdown::render_book(input, output_format = "bookdown::bs4_book", ...)
     
     # create a .nojekyll file which is needed to deploy on GitHub
     file.create(here::here("docs", ".nojekyll"))
   }
   
+  load("_knit_thesis_output_format.rda")
   if ("gitbook" %in% output_format){
-    bookdown::render_book(input, output_format = "bookdown::gitbook")
+    bookdown::render_book(input, output_format = "bookdown::gitbook", ...)
     
     # create a .nojekyll file which is needed to deploy on GitHub
     file.create(here::here("docs", ".nojekyll"))
   }
   
+  load("_knit_thesis_output_format.rda")
   if ("word" %in% output_format){
-    bookdown::render_book(input, output_format = "bookdown::word_document2")
+    bookdown::render_book(input, output_format = "bookdown::word_document2", ...)
   }
   
   # remove the _bookdown_files folder after the build
   unlink("_bookdown_files", recursive = TRUE)
+  unlink("_knit_thesis_output_format.rda")
 }
